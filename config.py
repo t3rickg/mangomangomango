d9d2a6f7b588c65fb0b74c338386d330dd4238a1
Adarsh986 <151984288+Adarsh986@users.noreply.github.com>
2024-03-31 19:35:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -102,13 +102,13 @@ def time_to_seconds(time):
 DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
 
 
-if SUPPORT_CHANNEL:https://t.me/indian_best_english_chatting
+if SUPPORT_CHANNEL:
     if not re.match("(?:http|https)://", SUPPORT_CHANNEL):https://t.me/indian_best_english_chatting
         raise SystemExit(
             "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
         )
 
-if SUPPORT_CHAT:https://t.me/indian_best_english_chatting
+if SUPPORT_CHAT:
     if not re.match("(?:http|https)://", SUPPORT_CHAT):https://t.me/indian_best_english_chatting
         raise SystemExit(
             "[ERROR] - Your SUPPORT_CHAT url is wrong. Please ensure that it starts with https://"
