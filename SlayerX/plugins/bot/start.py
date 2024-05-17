2e404496d2fc45f991b42050acb4eec2558a74c1
Mr Broken <74880754+asad0111@users.noreply.github.com>
2024-05-17 14:03:29 +0530

    Update start.py

--- SlayerX/plugins/bot/start.py
+++ SlayerX/plugins/bot/start.py
@@ -46,7 +46,7 @@ async def start_pm(client, message: Message, _):
                 )
             return
         if name[0:3] == "inf":
-            m = await message.reply_text("🔎")
+            m = await message.reply_text("🔮")
             query = (str(name)).replace("info_", "", 1)
             query = f"https://www.youtube.com/watch?v={query}"
             results = VideosSearch(query, limit=1)
